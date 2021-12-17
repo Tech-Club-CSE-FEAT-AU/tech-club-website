03fb18da22e1fc7c349f8532752d73ca6f38edfd
DIVAKARV08 <divakarv@Divakar-MacBook-Air.local>
2021-12-17 17:44:34 +0530

    day 4

---
+++ build/precache-manifest.2a745b776e5fab7b0ef6549cd876a7f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6af20392acf563b45cf0",
+    "url": "/tech-club-website/static/css/main.2f985da5.chunk.css"
+  },
+  {
+    "revision": "6af20392acf563b45cf0",
+    "url": "/tech-club-website/static/js/main.6af20392.chunk.js"
+  },
+  {
+    "revision": "de4e90b0334940b7cfd4",
+    "url": "/tech-club-website/static/css/1.4cd7d03d.chunk.css"
+  },
+  {
+    "revision": "de4e90b0334940b7cfd4",
+    "url": "/tech-club-website/static/js/1.de4e90b0.chunk.js"
+  },
+  {
+    "revision": "a76abd66e98bbebd9094",
+    "url": "/tech-club-website/static/js/runtime~main.a76abd66.js"
+  },
+  {
+    "revision": "cd64dfb126ae336d7e59205f8c2c0f90",
+    "url": "/tech-club-website/index.html"
+  }
+];
