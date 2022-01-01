c898924ad04af31978d340af4b7511374447bc8e
DIVAKARV08 <divakarv@Divakar-MacBook-Air.local>
2022-01-01 12:35:51 +0530

    pages deploymeny

--- src/components/EventsContainer/CardUi.js
+++ src/components/EventsContainer/CardUi.js
@@ -1,24 +1,54 @@
 import React from "react";
-import projectphoto1 from "../../Images/25-11-21 Workshop/Picture 1.png";
-import projectphoto2 from "../../Images/25-11-21 Workshop/Picture 2.png";
-import projectphoto3 from "../../Images/25-11-21 Workshop/Picture 3.png";
-import projectphoto4 from "../../Images/25-11-21 Workshop/Picture 4.png";
-
 import "./CradUi.css";
 
 const CardUi = (props) => {
   let metadata = props.metadata;
   return (
-    // <div className="cardbody" style={{ width: "18rem" }}>
-      <div className="card text-center">
-        <div className="overflow">
-          <img src={projectphoto1} alt="Event image" className="card-img-top" />
-        </div>
-        <div className="card-body text-dark">
-        <h4 className="card-title">{metadata.name}</h4>
-        <p className="card-text text-secondary">{metadata.description}</p>
+    // <div className="card-group" >
+    <div className="card text-center">
+      <div className="overflow">
+        <div id="demo" class="carousel slide" data-ride="carousel">
+          <ul class="carousel-indicators">
+            <li data-target="#demo" data-slide-to="0" class="active"></li>
+            <li data-target="#demo" data-slide-to="1"></li>
+            <li data-target="#demo" data-slide-to="2"></li>
+          </ul>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img
+                src={metadata.imgurl1}
+                alt="Event image"
+                className="card-img-top embed-responsive-item"
+              />
+            </div>
+            <div class="carousel-item">
+              <img
+                src={metadata.imgurl2}
+                alt="Event image"
+                className="card-img-top embed-responsive-item"
+              />
+            </div>
+            <div class="carousel-item">
+              <img
+                src={metadata.imgurl3}
+                alt="Event image"
+                className="card-img-top embed-responsive-item"
+              />
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#demo" data-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+          </a>
+          <a class="carousel-control-next" href="#demo" data-slide="next">
+            <span class="carousel-control-next-icon"></span>
+          </a>
         </div>
       </div>
+      <div className="card-body text-dark">
+        <h4 className="card-title">{metadata.name} </h4>
+        <p className="card-text text-secondary">{metadata.description}</p>
+      </div>
+    </div>
     // </div>
   );
 };
