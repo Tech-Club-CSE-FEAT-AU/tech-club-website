289c01d95dfdefdc3beaf52d5d4a8602f1998c14
DIVAKARV08 <divakarv@Divakar-MacBook-Air.local>
2022-01-04 21:19:17 +0530

    material icon

--- src/components/TopContainer/TopContainer.js
+++ src/components/TopContainer/TopContainer.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Element } from "react-scroll";
 import TopContent from "../TopContent/TopContent";
 import "./TopContainer.css";
-import "font-awesome/css/font-awesome.min.css";
+// import { AccessAlarm, ThreeDRotation } from "@mui/icons-material";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 const TopContainer = (props) => {
   let metadata = props.metadata;
@@ -14,10 +15,12 @@ const TopContainer = (props) => {
         {/* <hr /> */}
         <div className="Icons">
           <a href={metadata.github}>
-            <i class="fab fa-github"></i>
+            {/* <i class="fab fa-github"></i> */}
+            <GitHubIcon sx={{ color: "white", fontSize: 40 }} />
           </a>
           <a href={metadata.discord}>
             <i class="fab fa-discord"></i>
+            {/* <DiscordIcon /> */}
           </a>
         </div>
       </div>
--- src/components/WorkContainer/ProjectCard.js
+++ src/components/WorkContainer/ProjectCard.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./WorkContainer.css";
+import projectphoto from "../../Images/unknown copy.png";
 
 const ProjectCard = (props) => {
   let metadata = props.metadata;
   return (
-    <div >
+    <div>
       <div className="Workcontainer__projects">
         <div className="content">
           <h4 className="projectname">{metadata.name}</h4>
@@ -12,11 +13,7 @@ const ProjectCard = (props) => {
           <p>{metadata.content}</p>
         </div>
         <div className="img" class="container">
-          <img
-            src={projectphoto}
-            className="img-fluid"
-            alt="project name"
-          />
+          <img src={projectphoto} className="img-fluid" alt="project name" />
         </div>
       </div>
       <hr />
